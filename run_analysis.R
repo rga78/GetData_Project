718a6edab1a797d16bcbf6d879f260521e2fdfca
rga78 <robertgalderman@gmail.com>
2015-02-19 21:47:50 -0500

    read from pwd

--- run_analysis.R
+++ run_analysis.R
@@ -13,17 +13,17 @@
 
 # Read in subject data, activity data, and measurement data, and combine them all
 # into a single data frame.
-test.sub <- read.csv("data/test/subject_test.txt", header=F)
-test.activity <- read.csv("data/test/y_test.txt", header=F)
-test.X <- read.table("data/test/X_test.txt", header=F, sep="")
+test.sub <- read.csv("UCI HAR Dataset/test/subject_test.txt", header=F)
+test.activity <- read.csv("UCI HAR Dataset/test/y_test.txt", header=F)
+test.X <- read.table("UCI HAR Dataset/test/X_test.txt", header=F, sep="")
 
 test.X$subject <- test.sub$V1
 test.X$activity <- test.activity$V1
 
 # Do the same for the training data.
-train.sub <- read.csv("data/train/subject_train.txt", header=F)
-train.activity <- read.csv("data/train/y_train.txt", header=F)
-train.X <- read.table("data/train/X_train.txt", header=F, sep="")
+train.sub <- read.csv("UCI HAR Dataset/train/subject_train.txt", header=F)
+train.activity <- read.csv("UCI HAR Dataset/train/y_train.txt", header=F)
+train.X <- read.table("UCI HAR Dataset/train/X_train.txt", header=F, sep="")
 
 train.X$subject <- train.sub$V1
 train.X$activity <- train.activity$V1
@@ -36,7 +36,7 @@ X.df <- rbind(train.X, test.X)
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
 
 # Read the features list and find those labeled "mean()" and "std()"
-features <- read.table("data/features.txt", header=F, sep="")
+features <- read.table("UCI HAR Dataset/features.txt", header=F, sep="")
 features.mean <- subset(features, grepl("mean\\(\\)", V2))    
 features.std <- subset(features, grepl("std\\(\\)", V2))    
 
@@ -54,7 +54,7 @@ names(X.ms) <- append(colNames, c("subject", "activity"))
 #--------------------------------------------------------------------------------------------------
 # 3. Uses descriptive activity names to name the activities in the data set
 
-activities <- read.table("data/activity_labels.txt", header=F, sep="")
+activities <- read.table("UCI HAR Dataset/activity_labels.txt", header=F, sep="")
 X.ms$activity <- as.factor(X.ms$activity)
 levels(X.ms$activity) <- activities$V2
 
